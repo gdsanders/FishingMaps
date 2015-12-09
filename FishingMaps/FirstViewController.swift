dc235136a9411bb6621644fd49c190c9c8aafab1
gdsanders <gd@digitalequity.com>
2015-12-09 16:17:08 -0600

    Initial Commit

---
+++ FishingMaps/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FishingMaps
+//
+//  Created by G.D. Sanders on 12/9/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
