012f02265f38ffc16cff78122421db35e0143986
gdsanders <gd@digitalequity.com>
2016-02-23 21:24:33 -0600

    Outlets added

--- FishingMaps/CurrentLocationViewController.swift
+++ FishingMaps/CurrentLocationViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    @IBAction func getLocation() {
+        // do nothing yet
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +29,8 @@ class CurrentLocationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
